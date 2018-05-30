a95352321e8280447cda1202c93d7cc409f1c2de
serge <gyrsergey@gmail.com>
2018-05-30 20:06:10 +0400

    with '#' will be ignored, and an empty messae aborts the commit.

---
+++ src/MathVerify.php
@@ -0,0 +1,17 @@
+<?php
+
+
+namespace MathVerify;
+
+class MathVerify
+{
+	public function VerifyString($str)
+	{
+		if(strlen($str) == 0) 
+			return false;
+		if(substr_count($str, "(")==substr_count($str, ")"))
+			return true;
+		else
+			return false;
+	}
+}
